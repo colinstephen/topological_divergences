41f1201f8e173453000314bc1b02fa5abe8a5307
Colin <ab5169@coventry.ac.uk>
2023-09-04 12:07:53 +0100

    ensure supervised learning only uses finite features

--- lca_supervised_learning.py
+++ lca_supervised_learning.py
@@ -14,18 +14,24 @@ RANDOM_STATE = RandomState(MT19937(SeedSequence(SEED)))
 
 def score_classification(X, y):
     """Binary classifier for y>0 (chaos detector)."""
+    finite_idx = np.isfinite(X[:,0])
+    X, y = X[finite_idx], y[finite_idx]
     clf = RFC(random_state=RANDOM_STATE, n_estimators=50)
     scores = cross_val_score(clf, X, y>0, cv=10, scoring="f1")
     return np.mean(scores)
 
 def score_regression(X, y):
     """Regressor for y (chaos quantifier)."""
+    finite_idx = np.isfinite(X[:,0])
+    X, y = X[finite_idx], y[finite_idx]
     clf = RFR(random_state=RANDOM_STATE, n_estimators=50)
     scores = cross_val_score(clf, X, y, cv=10, scoring="neg_mean_absolute_error")
     return np.mean(scores)/iqr(y)
 
 def score_regression_pos(X, y):
     """Regressor for positive y (chaos quantifier)."""
+    finite_idx = np.isfinite(X[:,0])
+    X, y = X[finite_idx], y[finite_idx]
     clf = RFR(random_state=RANDOM_STATE, n_estimators=50)
     pos_mask = y > 0
     scores = cross_val_score(clf, X[pos_mask,:], y[pos_mask], cv=10, scoring="neg_mean_absolute_error")
